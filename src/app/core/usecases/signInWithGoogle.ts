020a50146c1cca8d2f92397054a6d71a59ac9a75
fleonel <dr04860@gmail.com>
2025-05-10 14:53:18 -0600

    feat: corrección de backend y logueo con tokens
    Este release ahora permite una conexión segura a la bd, genera un token
    que permite mantener una sesión activa sin necesidad de loguearse a cada
    rato.

--- src/app/core/usecases/signInWithGoogle.ts
+++ src/app/core/usecases/signInWithGoogle.ts
@@ -10,7 +10,7 @@ export class SignInWithGoogle {
       throw new Error("No se pudo autenticar con Google");
     }
 
-    const response = await fetch("/api/auth/token", {
+    const response = await fetch("../../api/auth/generate-token/", {
       method: "POST",
       headers: {
         "Content-Type": "application/json",
