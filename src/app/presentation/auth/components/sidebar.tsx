dfbc645e8c9346f39329de9cbd47854ebfa96aaa
WalterMijangos <walter24mijangosp@gmail.com>
2025-05-13 20:37:47 -0600

    feat: Creacion de componente de Sidebar Closes #36

---
+++ src/app/presentation/auth/components/sidebar.tsx
@@ -0,0 +1,76 @@
+"use client";
+
+import { useLoggedUserData } from "@/shared/hooks/useLoggedUserData";
+import React from "react";
+import { FiCalendar, FiList, FiFileText, FiLogOut, FiUser, FiSettings } from "react-icons/fi";
+import { useRouter } from "next/navigation";
+import Link from "next/link";
+
+const Sidebar: React.FC = () => {
+  const router = useRouter();
+  const user = useLoggedUserData();
+
+  const handleLogout = () => {
+    {/* Aqui regresa o limpia el token que da cuando se inicia sesion */}
+    router.push("/");
+  };
+
+  return (
+    
+    <div className="h-screen w-64 bg-black text-white p-4 flex flex-col relative">
+      {/* Boton para ajustes, pendiente la creacion de pagina y enlazar. Y corregir el nombre para enlazar*/}
+      <Link href="#" className="absolute top-4 right-4 text-white hover:text-gray-400">
+        <FiSettings size={20} />
+      </Link>
+
+
+      {/* Información del usuario */}
+      <div className="mt-10 text-center">
+
+        {/* Recupera la foto del usuario registrado */}
+        {user?.user_pic && user.user_pic.trim() !== "" ? (
+          <img
+            src={user.user_pic}
+            alt="Foto de perfil"
+            className="w-20 h-20 mx-auto mb-2 rounded-full object-cover"
+          />
+        ) : (
+          <FiUser size={36} className="mx-auto mb-2 text-gray-400" />
+        )}
+
+        {/* Recupera el nombre del usuario registrado */}
+        <h2 className="text-lg font-semibold">{user?.name || "Cargando..."}</h2>
+
+        {/* Recupera el correo del usuario registrado */}
+        <p className="text-sm text-gray-400">{user?.email || "..."}</p>
+      </div>
+
+      {/* Navegación */}
+      <nav className="mt-10 flex flex-col gap-4">
+        <Link href="/presentation/auth/dashboard" className="flex items-center gap-2 hover:text-gray-300">
+          <FiCalendar /> Mi día
+        </Link>
+        {/* Pendiente de creacion de la pagina tareas y enlazarY corregir el nombre para enlazar*/}
+        <Link href="#" className="flex items-center gap-2 hover:text-gray-300">
+          <FiList /> Tareas
+        </Link>
+        {/* Pendiente de creacion de la pagina notas y enlazar. Y corregir el nombre para enlazar*/}
+        <Link href="#" className="flex items-center gap-2 hover:text-gray-300">
+          <FiFileText /> Notas
+        </Link>
+      </nav>
+
+      {/* Botón de cerrar sesión */}
+      {/* Redireccionamiento al login funcional */}
+      <div className="mt-auto pt-6">
+        <button
+          onClick={handleLogout}
+          className="w-full flex items-center justify-center gap-2 bg-gray-800 hover:bg-gray-700 p-2 rounded">
+          <FiLogOut /> Cerrar sesión
+        </button>
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;
--- src/app/presentation/auth/dashboard/page.tsx
+++ src/app/presentation/auth/dashboard/page.tsx
@@ -1,6 +1,7 @@
 "use client";
 
 import { useLoggedUserData } from "@/shared/hooks/useLoggedUserData";
+import SidebarLayout from "../components/sidebarLayout";
 
 export default function WelcomePage() {
   const user = useLoggedUserData();
@@ -9,20 +10,22 @@ export default function WelcomePage() {
     return <p className="text-center mt-20 text-gray-500">Cargando...</p>;
 
   return (
-    <div className="flex flex-col items-center justify-center h-screen bg-[#ffffff] text-[#171717]">
-      <div className="bg-[#88a699] p-8 rounded-xl shadow-lg text-center max-w-md w-full">
-        <h1 className="text-3xl font-bold text-[#2d4343] mb-4">
-          ¡Hola, {user.name} 👋!
-        </h1>
-        <p className="text-[#171717] text-lg">
-          Pronto verás algo hermoso aquí...{" "}
-          <span className="italic">espéralo 😉</span>
-        </p>
-        <div className="mt-4 text-sm text-[#6f8b8f]">
-          <p>Correo: {user.email}</p>
-          <p>Carrera: {user.carrera}</p>
+    <SidebarLayout >
+      <div className="flex flex-col items-center justify-center h-screen bg-[#ffffff] text-[#171717]">
+        <div className="bg-[#88a699] p-8 rounded-xl shadow-lg text-center max-w-md w-full">
+          <h1 className="text-3xl font-bold text-[#2d4343] mb-4">
+            ¡Hola, {user.name} 👋!
+          </h1>
+          <p className="text-[#171717] text-lg">
+           Pronto verás algo hermoso aquí...{" "}
+           <span className="italic">espéralo 😉</span>
+          </p>
+          <div className="mt-4 text-sm text-[#6f8b8f]">
+            <p>Correo: {user.email}</p>
+            <p>Carrera: {user.carrera}</p>
+          </div>
         </div>
       </div>
-    </div>
+    </SidebarLayout >
   );
 }
